dd857bdbb6c97377ade9c744121411553f3a8f81
zah-ara <161930119+zah-ara@users.noreply.github.com>
2024-05-15 05:18:11 -0700

    push my cli-nmuber-guessing-game to github

---
+++ main.js
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+//1) computer will generate a random number. -DONE
+//2) user input for guessing number. -DONE
+//3) compare user input with computer gnerated and show result. -DONE
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("Welcome to the number guessing game");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    }
+]);
+if (answers.userguessednumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number.");
+}
+else {
+    console.log("Aww! You guessed the wrong number.");
+}
